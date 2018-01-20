81a7cda3478e174ca2c9dad40b4ef68471931004
Manuel Sousa <manuelsousamvs@gmail.com>
2018-01-20 01:30:20 +0000

    commit

--- 9.py
+++ 9.py
@@ -1,19 +1,19 @@
-def agente_corredor_ex_9():
+def Agente_corredor_ex_9():
     def __init__(self):
-        self.casaslimpas = 0
         self.cur = 1
 
-    def invoca(self,p):
-        if p == 0:
+    def invoca(self):
+        pos = 0
+        if pos == 0:
             if(self.cur == 0):
                 return "fica parado"
-        if(self.cur > p and self.cur > 1):
-                self.cur = self.cur - 1
-                return "andar-"
-        if(self.cur < p and self.cur < 8):
-                self.cur = self.cur + 1
-                return "andar+"
+        if(self.cur > pos and self.cur > 1):
+            self.cur = self.cur - 1
+            return "andar-"
+        if(self.cur < pos and self.cur < 8):
+            self.cur = self.cur + 1
+            return "andar+"
+        return ""
 
-a_e7 = agente_corredor_ex_9()
-a_e7.invoca(4)
-a_e7.invoca(8)
+
+asd = Agente_corredor_ex_9()
